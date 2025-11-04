c671fef172c0447a3bbc5ce7ddb23bb48a97f5a6
ramanakurva164 <ramana1642@gmail.com>
2025-11-04 14:05:56 +0530

    Update ai_agent.py

--- ai_agent.py
+++ ai_agent.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel("gemini-2.0-flash")
 
